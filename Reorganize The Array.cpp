a4935d54dc937a6e3236a3acc3a6d137d633ce06
sadiaprova12 <sadiaprova51717@gmail.com>
2024-10-11 16:22:55 +0600

    solved Reorganize The Array

---
+++ Reorganize The Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> rearrange(const vector<int>& arr) {
+        //code here
+        int n = arr.size();
+        vector<int> result(n, -1);
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= 0 && arr[i] < n) {
+                result[arr[i]] = arr[i];
+            }
+        }
+        
+        return result;
+    }
+};
